ff904f2255a6b5957245e3cb542ebdc7a9b9ce73
Ray Guo <ma2808203259@hotmail.com>
2021-05-05 17:19:46 +0800

    20 animal geo data

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 //mongodb uri
 //const uri = "mongodb+srv://ray:1998@cluster0.ho33k.mongodb.net/Sitboard?retryWrites=true&w=majority";
-const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/Sitboard?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/test?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
     useUnifiedTopology: true,
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, {
 let journalsCollection
 const openConnection = () => {
   client.connect(err => {
-    journalsCollection = client.db("Sitboard").collection("journals");
+    journalsCollection = client.db("test").collection("testGeo");
     if (!err) {
       console.log('connected to the database');
     }
@@ -34,5 +34,5 @@ app.get('', function(req, res){
 openConnection()
 
 app.listen(PORT,()=>{
-    console.log('server started on port 3000')
+    console.log('server started on port: ' + PORT)
 })
