73168a53d1f9590bf3e8cf3f1f08420b543c6638
Ray Guo <ma2808203259@hotmail.com>
2021-05-08 20:17:42 +0800

    finished to get map working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dir = './geojson';
 const fileFormat = '.geojson'
 
 const app = express()
-const PORT = 3000
+const PORT = 3000||process.env.PORT
 //mongodb uri
 //const uri = "mongodb+srv://ray:1998@cluster0.ho33k.mongodb.net/Sitboard?retryWrites=true&w=majority";
 const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/test?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
@@ -42,7 +42,26 @@ app.get('/insertData', function (req, res) {
   
 
 })
+//if the admin want to drop the database
+app.get('/dropDB', function(req,res){
+  let password = encodeURI(req.query.password)
+  var rightPassword = "75dfjejg55djejg"
+  if (password==rightPassword) {
+    geoCollection.drop(function(err, result){
+      console.log("Error : "+err);
+      if (err) throw err;
+      console.log("Operation Success ? "+result);
+      res.send("drop success")
+      
+  });
+  } else {
+    res.send('drop failed')
+  }
+
+})
+
 app.get('/find', function(req, res){
+  
   let name = encodeURI(req.query.name);
   findOneData(name, geoCollection,res)
  
@@ -57,8 +76,10 @@ const insertMany = (res) => {
   fileNames.forEach(element => {
     var jsonFile = JSON.parse(fs.readFileSync(dir + '/' + element))
     var fileName = element.substring(0, element.indexOf("."))
+    var resultName = fileName.replace(/\s/g,'')
     var payload = {
-      name: fileName,
+      name: resultName,
+      binomial: jsonFile.features[0].properties.BINOMIAL.replace(/\s/g,''),
       data: jsonFile
     }
 
@@ -92,8 +113,16 @@ const findOneData = async (animalName,collection,res)=>{
   };
 
   var result = await collection.findOne(query, options);
-  console.log(result);
-  res.send(result)
+
+  console.log(result.data.features[0].properties.BINOMIAL);
+
+  if (result == null) {
+    await res.send("404 not found")
+  }
+  else{
+    await res.send(result)
+
+  }
 
 }
 
